b0e3471f6fed1266a6531691ea866e173bf52ff8
Juan Manuel Incaurgarat <juanofx@gmail.com>
2020-07-20 11:55:47 +0200

    first commit

---
+++ resources/views/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+
+  @if (! have_posts())
+    <x-alert type="warning">
+      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+    </x-alert>
+
+    <div class="bg-white md:mt-4"></div>
+
+    {!! get_search_form(false) !!}
+  @endif
+@endsection
